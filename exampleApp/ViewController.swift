d134ada8c859bcface9fb3547d15409252087993
Kishore Kankata <kkankata@rapidbizapps.com>
2018-06-24 09:55:05 +0530

    This is my first commit

---
+++ exampleApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  exampleApp
+//
+//  Created by Kishore Kankata on 6/24/18.
+//  Copyright © 2018 rapidBizApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //adkfajdfalfkd
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
